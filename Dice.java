6ab3eec82e7e0868ff1217a5343c97a7ec4f0f85
若菜一保 <wanappo@wlan-napt-002.u-aizu.ac.jp>
2019-07-11 16:38:23 +0900

    First

--- Dice.java
+++ Dice.java
@@ -2,7 +2,6 @@ import java.util.Random;
 
 public class Dice{
   public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
     int sum = 0;
 
     Random r = new Random();
@@ -11,12 +10,7 @@ public class Dice{
     int D2 = r.nextInt(6) + 1;
 
     sum = D1 + D2;
-    System.out.println("Rolling the Dice...")
     System.out.println("Dice1: "+ D1 + ", Dice2: "+ D2);
     System.out.println("Sum = " + sum);
-    if(sum >= 8){
-      System.out.println(name + " Win!");
-    }
-    else {System.out.println(name + " Lose!");}
   }
 }
