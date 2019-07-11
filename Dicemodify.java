9470a850c2157cf70fca4a3cbc87c1bb9841a3a0
若菜一保 <wanappo@wlan-napt-002.u-aizu.ac.jp>
2019-07-11 16:27:35 +0900

    Ex4 modify1

---
+++ Dicemodify.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dicemodify{
+  public static void main(String[] args){
+
+    System.out.println("What's your name?");
+    Scanner scan = new Scanner(System.in);
+    String name = scan.next();
+
+    System.out.println("Hello! " + name + "!");
+
+  }
+}
